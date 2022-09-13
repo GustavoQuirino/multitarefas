e381fc68cbe72097a2f5b805f98a405743fd56d4
GustavoQuirino <gh.quirino@yahoo.com>
2022-09-12 21:00:04 -0300

    criado relogio

--- src/pages/Timer.jsx
+++ src/pages/Timer.jsx
@@ -1,12 +1,26 @@
+import { useEffect, useState } from 'react'
 import './Timer.css'
 
 
 const Timer = () => {
 
+    const d = new Date()
+    const hora = d.getHours()
+    const minutos = d.getMinutes()
+    const [segundos ,setSegundos] = useState()
+    
+    function pegaSegundos() {
+        setSegundos(d.getSeconds())
+    }
+
+    
+
     return (
         <div className="principal">
             <div className="relogio">
-                
+                {<h1>{hora}</h1>}
+                {<h1>{minutos}</h1>}
+                {<h1>{segundos}</h1>}
             </div>
             <div className="timer">
 
